6fe6a539f5daff948ac0faa64b7a7cd5614e6b24
Yomna Magdy Soliman <36536078+YomnaMagdy@users.noreply.github.com>
2019-05-10 08:29:41 -0700

    Create LCD_PutsInt.c

---
+++ LCD/LCD_PutsInt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#include <stdint.h>
+
+#include "MCAL.h"
+
+#include "LCD.h"
+
+
+
+
+
+void LCD_PutsInt(int32_t num){
+
+	char buffer[8];
+
+	sprintf(buffer,"%d", num);
+
+	LCD_Puts(buffer);
+
+}
