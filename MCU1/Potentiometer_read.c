d8e672e693aa7f50e2313168ebaa7170fd6de6c9
aliamaim <ali.amaim@yahoo.com>
2019-05-15 05:08:22 +0200

    Used to read sampled values of potentiometer.

---
+++ MCU1/Potentiometer_read.c
@@ -0,0 +1,9 @@
+uint16_t Potentiometer_read(void){
+	
+	 ADC0_PSSI_R = ADC0_PSSI_SS3;
+	while((ADC0_RIS_R& ADC0_RIS_INR3)!=ADC0_RIS_INR3);	//polling on bit INR3 for sequencer 3 till be assigned to end conv
+	ADC0_ISC_R = ADC0_RIS_INR3;                    //clear bit INR3 by assign 1 in bit 3 in isc
+	return (ADC0_SSFIFO3_R&0xFFF);                  //return value on fifo
+	
+	
+}
