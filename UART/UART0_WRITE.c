5a0c46e179d72af1718de29858ed2bf9f774e1ce
a7medfawzy97 <36432072+a7medfawzy97@users.noreply.github.com>
2019-05-15 15:31:39 +0200

    Update UART0_WRITE.c

--- UART/UART0_WRITE.c
+++ UART/UART0_WRITE.c
@@ -4,7 +4,7 @@
 
 void UART0_WRITE(uint8_t data){
 	
-	while ((UART0_FR_R&UART_FR_TXFF)!=0);  // AS LONG AS THE FIFO IS FULL -->DO NOTHING 
+	while ((UART0_FR_R&UART_FR_TXFF)!=0);  // As LONG AS THE FIFO IS FULL -->DO NOTHING 
 	
 	UART0_DR_R = data;
 	
