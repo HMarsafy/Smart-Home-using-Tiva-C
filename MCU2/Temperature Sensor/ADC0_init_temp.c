296002cbe6d2a93f7325f83ec5309418f8d9efa4
Yomna Magdy Soliman <36536078+YomnaMagdy@users.noreply.github.com>
2019-05-15 17:22:18 -0700

    Update ADC0_init_temp.c

--- MCU2/Temperature Sensor/ADC0_init_temp.c
+++ MCU2/Temperature Sensor/ADC0_init_temp.c
@@ -1,3 +1,35 @@
+#include "tm4c123gh6pm.h"
+#include "LCD.h"
+#include "temp_sensor.h"
+#include <stdint.h>
+
+
+#define ADC0_SS0_PRI 			0x3
+
+#define ADC0_SS1_PRI 			(0x2 << 4)
+
+#define ADC0_SS2_PRI 			(0x1 << 8)
+
+#define ADC0_SS3_PRI 			(0x0 << 12)
+
+#define ADC0_ACTSS_SS3     0x8
+
+#define ADC0_EMUX_SS3			(0x0 << 12)                                              
+
+#define ADC0_SSMUX3				 0x0	                                                  
+
+#define ADC0_SSCTL3_IE0		(0x1 << 2)
+
+#define ADC0_SSCTL3_END0	(0x1 << 1)
+
+#define GPIO_PE3_M         (1  << 4)
+
+#define ADC0_PSSI_SS3			(0x1 << 3)
+
+#define ADC0_RIS_INR3			(0x1 << 3)
+
+#define ADC0_SSCTL3_TS0    (1  << 3 )
+
 void ADC0_init_temp(void)
 {
 	volatile int delay;
